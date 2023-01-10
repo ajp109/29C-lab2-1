b8b87e046aab35719ee3fff19141bfdfc69d12c9
ajp109 <andrew.pomfret@york.ac.uk>
2023-01-10 16:00:12 +0000

    Initial commit of project-specific files

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include "mbed.h"
 
 int main() {
+
+    int array[10];
+
+    array[4] = 5;
+
+    printf("The value stored in array[4] is %d\n", array[4]);
+
+    // Do not allow main() to end
     while (true) {
         sleep();
     }
